c5eef596b44755e30841926dd750672cc0bb6e3e
LireEruel <jsh001505@gmail.com>
2021-06-28 18:00:00 +0900

    기본 세팅 및 navigation 첫 적용

---
+++ src/components/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { AuthContext } from "../contexts/context";
+import ScreenContainer from "react-native-screens";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+});
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="React Native by Example"
+        onPress={() =>
+          navigation.push("Details", { name: "React Native by Example " })
+        }
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
